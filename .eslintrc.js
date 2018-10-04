7ca7770bb1aead91fd682283bda79373e9a1032d
ehnawh <ehnawh@gmail.com>
2018-10-04 20:47:48 +0900

    fix lint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,15 +1,16 @@
 module.exports = {
   root: true,
-  parser: "babel-eslint",
-  extends: ["eslint:recommended", "plugin:react-app/recommended"],
-  plugins: ["react-native", "prettier"],
+  parser: 'babel-eslint',
+  extends: ['eslint:recommended', 'plugin:react-app/recommended'],
+  plugins: ['react-native', 'prettier'],
   rules: {
-    "no-console": "off",
-    "no-debugger": 0,
-    curly: "warn",
-    "react-app/jsx-a11y/href-no-hash": "off",
-    "react-app/import/first": "off",
-    quotes: ["error", "single", { avoidEscape: true }]
+    'no-console': 'off',
+    'no-debugger': 0,
+    curly: 'warn',
+    'react-app/jsx-a11y/href-no-hash': 'off',
+    'react-app/import/first': 'off',
+    quotes: ['error', 'single', { avoidEscape: true }],
     // 'prettier/prettier': ['error']
+    code: 80
   }
 };
