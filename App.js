999764ee28e67b206cdb657cf6333ddd94427a78
ehnawh <ehnawh@gmail.com>
2018-10-04 20:51:34 +0900

    add test code

--- App.js
+++ App.js
@@ -1,21 +1,59 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { Component } from 'react';
+import { Platform, StatusBar, StyleSheet, View } from 'react-native';
+import { AppLoading, Asset, Font, Icon } from 'expo';
 
-export default class App extends React.Component {
+export default class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      isLoadingComplete: false
+    };
+  }
+
   render() {
     return (
-      <View style={styles.container}>
-        <Text>EOSID</Text>
-      </View>
+      <React.Fragment>
+        {!this.state.isLoadingComplete && !this.props.skipLoadingScreen ? (
+          <AppLoading
+            startAsync={() => this.startAsyncLoading}
+            onError={() => this.onErrorLoading()}
+            onFinish={() => this.onFinishLoading()}
+          />
+        ) : (
+          <View style={styles.container}>
+            {Platform.OS === 'ios' && <StatusBar barStyle="default" />}
+          </View>
+        )}
+      </React.Fragment>
     );
+  }
+
+  async startAsyncLoading() {
+    return Promise.all([
+      Asset.loadAsync(),
+      Font.loadAsync({
+        ...Icon.Ionicons.font
+      })
+    ]);
+  }
+
+  onErrorLoading(error) {
+    // In this case, you might want to report the error to your error
+    // reporting service, for example Sentry
+    console.warn(error);
+  }
+
+  onFinishLoading() {
+    this.setState({ isLoadingComplete: true });
   }
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    backgroundColor: '#fff'
+    // alignItems: 'center',
+    // justifyContent: 'center'
+  }
 });
