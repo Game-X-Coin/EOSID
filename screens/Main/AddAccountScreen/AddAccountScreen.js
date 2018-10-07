8035a15ad01f2cbcca0647ac9d268b1d55121560
Go Eun-Seok <gomonk3037@gmail.com>
2018-10-08 04:35:09 +0900

    Add add-account screen

---
+++ screens/Main/AddAccountScreen/AddAccountScreen.js
@@ -0,0 +1,174 @@
+import React, { Component } from 'react';
+import { observable } from 'mobx';
+import { observer, inject } from 'mobx-react';
+import { SafeAreaView, ScrollView, View } from 'react-native';
+import {
+  Appbar,
+  Button,
+  Dialog,
+  List,
+  Portal,
+  RadioButton
+} from 'react-native-paper';
+import { TextField } from 'react-native-material-textfield';
+import { Dropdown } from 'react-native-material-dropdown';
+import { withFormik } from 'formik';
+import * as Yup from 'yup';
+
+import { AccountError } from '../../../db';
+import { AccountService } from '../../../services';
+
+import HomeStyle from '../../../styles/HomeStyle';
+
+@inject('networkStore', 'accountStore')
+@withFormik({
+  mapPropsToValues: props => ({
+    // select account
+    showDialog: false,
+    accounts: [],
+    selectedAccount: '',
+    // form
+    privateKey: '',
+    publicKey: '',
+    networkId: props.networkStore.defaultNetworks[0].id
+  }),
+  validationSchema: props => {
+    const { errors } = AccountError.RequiredFields;
+
+    return Yup.object().shape({
+      privateKey: Yup.string().required(errors.privateKey),
+      networkId: Yup.string().required(errors.networkId)
+    });
+  },
+  handleSubmit: async (
+    values,
+    { props, setSubmitting, setValues, setErrors }
+  ) => {
+    try {
+      const publicKey = await AccountService.privateToPublic(values.privateKey);
+      const accounts = await AccountService.findKeyAccount(publicKey);
+
+      setValues({
+        ...values,
+        showDialog: true,
+        accounts,
+        publicKey
+      });
+    } catch (error) {
+      setErrors({ message: error.message, ...error.errors });
+      setSubmitting(false);
+    }
+  }
+})
+@observer
+export class AddAccountScreen extends Component {
+  async addAccount() {
+    const { accountStore, navigation, values } = this.props;
+
+    if (!values.selectedAccount) {
+      return;
+    }
+
+    await accountStore.addAccount({
+      name: values.selectedAccount,
+      privateKey: values.privateKey,
+      publicKey: values.publicKey,
+      networkId: values.networkId
+    });
+
+    navigation.navigate('Account');
+  }
+
+  selectAccount(account) {
+    this.props.setFieldValue('selectedAccount', account);
+  }
+
+  render() {
+    const {
+      navigation,
+      networkStore: { defaultNetworks, userNetworks },
+
+      values,
+      errors,
+      touched,
+      setFieldValue,
+      setFieldTouched,
+      handleSubmit,
+      isSubmitting
+    } = this.props;
+
+    const networks = [...defaultNetworks, ...userNetworks].map(network => ({
+      label: network.name,
+      value: network.id
+    }));
+
+    return (
+      <View style={HomeStyle.container}>
+        <SafeAreaView style={HomeStyle.container}>
+          <Appbar.Header>
+            <Appbar.BackAction onPress={() => navigation.goBack(null)} />
+            <Appbar.Content title={'Add eos account'} />
+          </Appbar.Header>
+          <ScrollView style={HomeStyle.container}>
+            <TextField
+              multiline
+              label="Private key"
+              style={{ fontFamily: 'monospace' }}
+              value={values.privateKey}
+              error={touched.privateKey && errors.privateKey}
+              onChangeText={_ => {
+                setFieldTouched('privateKey', true);
+                setFieldValue('privateKey', _);
+              }}
+            />
+
+            <Dropdown
+              label="Network"
+              data={networks}
+              value={values.networkId}
+              error={touched.networkId && errors.networkId}
+              onChangeText={_ => {
+                setFieldTouched('networkId', true);
+                setFieldValue('networkId', _);
+              }}
+            />
+
+            <Button loading={isSubmitting} onPress={() => handleSubmit()}>
+              Add account
+            </Button>
+
+            <Portal>
+              <Dialog visible={values.showDialog}>
+                <Dialog.Title>Select eos account</Dialog.Title>
+                <Dialog.Content>
+                  <List.Section>
+                    {values.accounts.map(account => (
+                      <List.Item
+                        key={account}
+                        title={account}
+                        right={() => (
+                          <RadioButton
+                            status={
+                              values.selectedAccount === account
+                                ? 'checked'
+                                : 'unchecked'
+                            }
+                            onPress={() => this.selectAccount(account)}
+                          />
+                        )}
+                        onPress={() => this.selectAccount(account)}
+                      />
+                    ))}
+                  </List.Section>
+                </Dialog.Content>
+                <Dialog.Actions>
+                  <Button onPress={() => this.addAccount()}>Done</Button>
+                </Dialog.Actions>
+              </Dialog>
+            </Portal>
+          </ScrollView>
+        </SafeAreaView>
+      </View>
+    );
+  }
+}
--- screens/Main/index.js
+++ screens/Main/index.js
@@ -1,3 +1,5 @@
 export * from './SettingsScreen';
 export * from './NetworkScreen';
 export * from './AddNetworkScreen';
+export * from './AddAccountScreen';
+export * from './AccountScreen';
