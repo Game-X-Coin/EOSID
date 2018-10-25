4e4439f6979a18446580d3982cd5d1479885b579
Go Eun-Seok <gomonk3037@gmail.com>
2018-10-25 15:10:30 +0900

    Separate views in transaction screen

--- screens/Main/TransactionScreen/TransactionScreen.js
+++ screens/Main/TransactionScreen/TransactionScreen.js
@@ -1,8 +1,10 @@
 import React, { Component } from 'react';
 import { observer, inject } from 'mobx-react';
-import { SafeAreaView, ScrollView, View } from 'react-native';
-import { Appbar, Text, Caption, List } from 'react-native-paper';
-import moment from 'moment';
+import { SafeAreaView } from 'react-native';
+import { Appbar } from 'react-native-paper';
+
+import { AccountEmptyState } from '../AccountScreen';
+import { TransactionInfo } from './TransactionInfo';
 
 import HomeStyle from '../../../styles/HomeStyle';
 
@@ -10,42 +12,21 @@ import HomeStyle from '../../../styles/HomeStyle';
 @observer
 export class TransactionScreen extends Component {
   render() {
-    const { actions } = this.props.accountStore;
+    const { currentUserAccount } = this.props.accountStore;
 
     return (
-      <View style={HomeStyle.container}>
-        <SafeAreaView style={HomeStyle.container}>
-          <Appbar.Header>
-            <Appbar.Content title="Transaction" />
-          </Appbar.Header>
-          <ScrollView style={HomeStyle.container}>
-            <List.Section title="Lastest">
-              {actions.map(({ block_time, action_trace }, i) => (
-                <List.Item
-                  key={i}
-                  title={
-                    <Text>
-                      {action_trace.act.name}{' '}
-                      <Caption>by {action_trace.act.account}</Caption>
-                    </Text>
-                  }
-                  // description={JSON.stringify(action_trace.act.data)}
-                  right={() => (
-                    <Caption style={{ alignSelf: 'center' }}>
-                      {moment(block_time).fromNow()}
-                    </Caption>
-                  )}
-                  onPress={() =>
-                    this.props.navigation.navigate('TransactionDetail', {
-                      txId: action_trace.trx_id
-                    })
-                  }
-                />
-              ))}
-            </List.Section>
-          </ScrollView>
-        </SafeAreaView>
-      </View>
+      <SafeAreaView style={HomeStyle.container}>
+        <Appbar.Header>
+          <Appbar.Content title="Transaction" />
+        </Appbar.Header>
+
+        {currentUserAccount ? (
+          <TransactionInfo />
+        ) : (
+          // account empty
+          <AccountEmptyState />
+        )}
+      </SafeAreaView>
     );
   }
 }
--- screens/Main/TransactionScreen/index.js
+++ screens/Main/TransactionScreen/index.js
@@ -1,2 +1,4 @@
 export * from './TransactionScreen';
+export * from './TransactionInfo';
+export * from './TransactionEmptyState';
 export * from './TransactionDetailScreen';
