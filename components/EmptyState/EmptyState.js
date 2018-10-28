ae175791df0e18f6186e532ea1f8008b503bf3c7
Go Eun-Seok <gomonk3037@gmail.com>
2018-10-28 20:11:16 +0900

    Add styles props to EmptyState component

--- components/EmptyState/EmptyState.js
+++ components/EmptyState/EmptyState.js
@@ -4,25 +4,33 @@ import { Title, Paragraph, Colors } from 'react-native-paper';
 
 export class EmptyState extends Component {
   render() {
-    const { image, title, description, children } = this.props;
-
-    const IMAGE_SIZE = 200;
+    const {
+      image,
+      title,
+      description,
+      children,
+      imageSize = 200,
+      imageStyle,
+      titleStyle,
+      descriptionStyle
+    } = this.props;
 
     return (
       <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
         <Image
-          style={{ width: IMAGE_SIZE, height: IMAGE_SIZE }}
+          style={{ width: imageSize, height: imageSize, ...imageStyle }}
           source={image}
         />
 
-        <Title style={{ marginBottom: 5 }}>{title}</Title>
+        <Title style={{ marginBottom: 5, ...titleStyle }}>{title}</Title>
 
         <Paragraph
           style={{
             marginBottom: 35,
             paddingHorizontal: 30,
             color: Colors.grey500,
-            textAlign: 'center'
+            textAlign: 'center',
+            ...descriptionStyle
           }}
         >
           {description}
