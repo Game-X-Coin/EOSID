6e3ae0941542eacfd4968f0ee82cb9358de934a9
ehnawh <ehnawh@gmail.com>
2018-11-26 04:56:51 +0900

    fix bug about save keys

--- services/AccountService.js
+++ services/AccountService.js
@@ -44,9 +44,11 @@ export default class AccountService {
 
     if (!account) {
       // create new account instance
-      const keys = [
-        { publicKey, encryptedPrivateKey, permission: permissions[0] }
-      ];
+      const keys = AccountService.setKey({
+        publicKey,
+        encryptedPrivateKey,
+        permission: permissions[0]
+      });
       account = new AccountModel({ ...accountInfo, name, keys });
       permissions = permissions.slice(1);
       // check duplicated permission in account
@@ -64,7 +66,7 @@ export default class AccountService {
         permission
       };
 
-      return AccountService.setKey(pv, key);
+      return AccountService.setKey(key, pv);
     }, []);
 
     if (keys.length) {
@@ -163,10 +165,7 @@ export default class AccountService {
     return replaced;
   }
 
-  static setKey(keys, key) {
-    if (!keys) {
-      keys = [];
-    }
+  static setKey(key, keys = []) {
     const stringifiedKey = AccountService.stringifyKey(key);
     keys.push(stringifiedKey);
     return keys;
